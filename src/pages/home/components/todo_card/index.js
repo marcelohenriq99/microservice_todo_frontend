f385a070236233875495e620012c8a445049e6ad
Marcelo Henrique <henrique19.marcelo@gmail.com>
2024-10-17 22:38:58 -0300

    feat(events): adding events logic and update data

--- src/pages/home/components/todo_card/index.js
+++ src/pages/home/components/todo_card/index.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './index.css';
 
-function TodoCard({ task }) {
+function TodoCard({ task, onEdit, onDelete }) {
+  const [editing, setEditing] = useState(false);
+  const [taskTitle, setTaskTitle] = useState(task.title);
 
   //events
+  function changeTitle(event){
+    setTaskTitle(event.target.value);
+  }
+
   function markAsDone(_) {
-    console.log('markAsDone', task);
+    console.log('markAsDone');
+    onEdit(
+      task.id,
+      {
+        ...task,
+        done: !task.done
+      },
+    );
   }
-  
+
   function editTask(_) {
-    console.log('edit');
+    setEditing(true);
   }
   
-  function deleteTask(_) {
-    console.log('delete');
+  function updateTask(_) {
+    console.log('edit');
+    onEdit(
+      task.id,
+      {
+        ...task,
+        title: taskTitle,
+      },
+    );
+    setEditing(false);
   }
 
   return (
@@ -30,27 +51,38 @@ function TodoCard({ task }) {
         <label className="form-check-label" htmlFor={task.id}>
         </label>
       </div>
-      <p className="task-title"> { task.title } </p>
-
-      <div className="dropdown ms-auto">
-        <button className="btn btn-primary todo-actions d-flex justify-content-center align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i className="bi bi-three-dots"></i>
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            <button className="dropdown-item" onClick={editTask}>
-              <i className="bi bi-pencil"></i>
-              <span className="px-2">Edit</span>
-            </button>
-          </li>
-          <li>
-            <button className="dropdown-item" onClick={deleteTask}>
-              <i className="bi bi-x-lg"></i>
-              <span className="px-2">Delete</span>
-            </button>
-          </li>
-        </ul>
-      </div>
+      { editing ? <input className="form-control form-control-sm mx-2 w-75" type="text" value={taskTitle} onChange={changeTitle} /> : <p className="task-title"> { taskTitle } </p> }
+      
+      {
+        editing ?
+        <>
+          <button className="btn btn-primary todo-actions d-flex justify-content-center align-items-center ms-auto mx-2" type="button" onClick={updateTask}>
+            <i className="bi bi-check2"></i>
+          </button>
+          <button className="btn btn-primary todo-actions d-flex justify-content-center align-items-center" type="button" onClick={() => setEditing(false)}>
+            <i className="bi bi-x-lg"></i>
+          </button>
+        </> :
+        <div className="dropdown ms-auto">
+          <button className="btn btn-primary todo-actions d-flex justify-content-center align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i className="bi bi-three-dots"></i>
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <button className="dropdown-item" onClick={editTask}>
+                <i className="bi bi-pencil"></i>
+                <span className="px-2">Edit</span>
+              </button>
+            </li>
+            <li>
+              <button className="dropdown-item" onClick={() => onDelete(task.id)}>
+                <i className="bi bi-x-lg"></i>
+                <span className="px-2">Delete</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      }
     </div>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TodoCard from './components/todo_card';
 
 import './index.css';
 
-const tasks = [
-  {
-    id: 1,
-    title: 'Create frontend with React',
-    done: false,
-  },
-  {
-    id: 2,
-    title: 'Create Auth service',
-    done: true,
-  },
-  {
-    id: 3,
-    title: 'Create notification service',
-    done: false,
-  },
-  {
-    id: 4,
-    title: 'Create scraping service',
-    done: true,
-  }
-];
-
 function Home() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Create frontend with React',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'Create Auth service',
+      done: true,
+    },
+    {
+      id: 3,
+      title: 'Create notification service',
+      done: false,
+    },
+    {
+      id: 4,
+      title: 'Create scraping service',
+      done: true,
+    }
+  ]);
+
+  //events
+  function editTask(taskId, taskData) {
+    setTasks(tasks.map(task =>
+      task.id === taskId ? { id: taskId, ...taskData } : task
+    ));
+    console.log(tasks);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  }
+
   return (
     <div className="container-fluid vh-100 w-100 todo-gradient-bg d-flex flex-column justify-content-center align-items-center p-5">
       <div className="row col-12 app-title">
@@ -37,7 +49,7 @@ function Home() {
       <ul className="row col-12">
         {tasks.map((item, index) => (
           <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
-            <TodoCard task={item} />
+            <TodoCard task={item} onEdit={editTask} onDelete={deleteTask} />
           </li>
         ))}
       </ul>
